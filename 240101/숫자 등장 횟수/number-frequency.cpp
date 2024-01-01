3a22ca8da75acabbbae821e34e549f2a3af1b083
ohmingi <87136887+ohmingi@users.noreply.github.com>
2024-01-01 18:23:47 +0900

    Title: 숫자 등장 횟수, Time: 1046ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

--- 240101/숫자 등장 횟수/number-frequency.cpp
+++ 240101/숫자 등장 횟수/number-frequency.cpp
@@ -16,13 +16,14 @@ int main(){
 		v.push_back(a);
 	}
 	
-	vector<int>::iterator iter=v.begin();
 	
 	while(m--){
 		int a;
 		int count=0;
 		
 		cin >> a;
+		
+		vector<int>::iterator iter=v.begin();
 	
 		for(iter;iter!=v.end();iter++){
 			if(*iter==a){
