e7f01be0487bdfffeb2ad3a10c1767f0fae7877e
ohmingi <87136887+ohmingi@users.noreply.github.com>
2024-04-21 17:03:41 +0900

    Title: 두 사람, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240421/두 사람/two-person.py
+++ 240421/두 사람/two-person.py
@@ -1,9 +1,9 @@
 arr=input().split()
 a=int(arr[0])
-b=char(arr[1])
+b=string(arr[1])
 
 arr1=input().split()
-a1,b1=int(arr1[0]),char(arr1[1])
+a1,b1=int(arr1[0]),string(arr1[1])
 
 if (a>=19 or a1>=19) and (b=='M' or b1=='M'):
     print(1)
