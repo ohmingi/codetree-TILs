4f10dbe89645e07dc83c3cf3f09445cb0f21be88
ohmingi <87136887+ohmingi@users.noreply.github.com>
2024-04-30 22:27:03 +0900

    Title: 코로나 메뉴얼, Time: 147ms, Memory: 24MB, Status: Passed - CodeTree

--- 240430/코로나 메뉴얼/covid-manual.py
+++ 240430/코로나 메뉴얼/covid-manual.py
@@ -1,18 +1,12 @@
 arr = input().split()
 a = int(arr[1])
 
-A,B,C=0,0,0
+A=0
 
 if arr[0]=='Y':
     if a>=37:
         A+=1
-    else:
-        C+=1
-else:
-    if a>=37:
-        B+=1
-    else:
-        D+=1
+
 
 arr1 = input().split()
 a1 = int(arr1[1])
@@ -20,13 +14,7 @@ a1 = int(arr1[1])
 if arr1[0]=='Y':
     if a1>=37:
         A+=1
-    else:
-        C+=1
-else:
-    if a1>=37:
-        B+=1
-    else:
-        D+=1
+
 
 
 arr2 = input().split()
@@ -35,13 +23,7 @@ a2= int(arr2[1])
 if arr2[0]=='Y':
     if a2>=37:
         A+=1
-    else:
-        C+=1
-else:
-    if a2>=37:
-        B+=1
-    else:
-        D+=1
+
 
 if A>=2:
     print('E')
