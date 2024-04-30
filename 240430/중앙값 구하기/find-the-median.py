fd92551d13700aa228718c46525e8caded7ba9ed
ohmingi <87136887+ohmingi@users.noreply.github.com>
2024-04-30 22:39:05 +0900

    Title: 중앙값 구하기, Time: 113ms, Memory: 25MB, Status: Passed - CodeTree

---
+++ 240430/중앙값 구하기/find-the-median.py
@@ -0,0 +1,19 @@
+arr=input().split()
+
+a,b,c=int(arr[0]),int(arr[1]),int(arr[2])
+
+if a>b and a>c:
+    if b>c:
+        print(b)
+    else:
+        print(c)
+elif b>a and b>c:
+    if a>c:
+        print(a)
+    else:
+        print(c)
+else:
+    if b>a:
+        print(b)
+    else:
+        print(a)
