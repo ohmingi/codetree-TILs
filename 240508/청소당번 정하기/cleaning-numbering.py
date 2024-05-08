30a7fe81de17989d1052792502745ca388524d24
ohmingi <87136887+ohmingi@users.noreply.github.com>
2024-05-08 22:16:01 +0900

    Title: 청소당번 정하기, Time: 109ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 240508/청소당번 정하기/cleaning-numbering.py
@@ -0,0 +1,13 @@
+cnt1,cnt2,cnt3=0,0,0
+
+a=int(input())
+
+for i in range(1,a+1):
+    if i%12==0:
+        cnt3+=1
+    elif i%3==0:
+        cnt2+=1
+    else:
+        cnt1+=1
+
+print(cnt1,cnt2,cnt3,end=" ")
