2167bbf296f66a80ebbd7f8dda98b810568660e0
ohmingi <87136887+ohmingi@users.noreply.github.com>
2024-06-07 16:36:16 +0900

    Title: 행복한 수열의 개수, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240607/행복한 수열의 개수/number-of-happy-sequence.cpp
+++ 240607/행복한 수열의 개수/number-of-happy-sequence.cpp
@@ -1,63 +1,32 @@
 #include <iostream>
-
 using namespace std;
 
 int main() {
-    int n,m;
+    int n, m;
     cin >> n >> m;
 
     int arr[n][n];
-    
-    for(int i=0;i<n;i++){
-        for(int t=0;t<n;t++){
-            int a;
-            cin >> a;
-            arr[i][t]=a;
+
+    // 배열 입력 받기
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < n; j++)
+            cin >> arr[i][j];
+
+    int count = 0;
+
+    // 각 행과 열에서 연속된 숫자가 m번 나타나는지 확인
+    for (int i = 0; i < n; i++) {
+        int cntRow = 1, cntCol = 1;
+        for (int j = 1; j < n; j++) {
+            if (arr[i][j] == arr[i][j - 1]) cntRow++;
+            else cntRow = 1;
+            if (arr[j][i] == arr[j - 1][i]) cntCol++;
+            else cntCol = 1;
+            if (cntRow == m) count++;
+            if (cntCol == m) count++;
         }
     }
 
-    int count=0;
-
-    /*if(m==1) {
-        cout << 2*n;
-        return 0;
-    }*/
-
-    for(int i=0;i<n;i++){
-        int cnt=1;
-        int cnt1=1;
-
-        bool b=false;
-        bool b1=false;
-
-        int c=arr[i][0];
-        int c1=arr[0][i];
-
-        for(int t=1;t<n;t++){
-            
-            if(c==arr[i][t]) cnt++;
-            else{
-                cnt=1;
-            }
-            if(c1==arr[t][i]) cnt1++;
-            else{
-                cnt1=1;
-            }
-
-            if(cnt==m && b==false) {
-                count++;
-                b=true;
-
-            }
-            if(cnt1==m && b1==false) {
-                count++;
-                b1=true;
-            }
-
-            c=arr[i][t];
-            c1=arr[t][i];
-        }
-    }
-    
     cout << count;
+    return 0;
 }
