a592ea169b6892533088b530c2f8cea37dadf313
ohmingi <87136887+ohmingi@users.noreply.github.com>
2024-07-09 22:54:14 +0900

    Title: 마을 구분하기, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240709/마을 구분하기/seperate-village.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+#define maxm 25
+
+bool grid[maxm][maxm];
+bool visited[maxm][maxm];
+
+int n;
+int c;
+vector<int>Count;
+int akdmf = 0;
+
+int dx[2] = {0,1};
+int dy[2] = {1,0};
+
+bool check(int x, int y){
+    return 0<=x && x<n && 0<=y && y<n;
+}
+
+void dfs(int x, int y){
+    for(int i=0 ; i<2 ;i++){
+        int new_x = x + dx[i];
+        int new_y = y + dy[i];
+
+        if(check(new_x,new_y) && visited[new_x][new_y] == 0 && grid[new_x][new_y] == 1){
+            c++;
+            visited[new_x][new_y] = 1;
+            /*cout << new_x <<" " << new_y <<"\n";
+            cout << c << "\n";*/
+            dfs(new_x,new_y);
+        }
+    }
+}
+
+int main() {
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        for(int t=0;t<n;t++){
+            int a;
+            cin >> a;
+
+            grid[i][t] = a;
+        }
+    }
+
+    for(int i=0;i<n;i++){
+        for(int t=0;t<n;t++){
+           if(grid[i][t] == 1 && visited[i][t] == 0){
+                c = 1;
+                visited[i][t] = 1;
+                dfs(i,t);
+                Count.push_back(c);
+                akdmf++;
+           }
+        }
+    }
+
+    cout << akdmf << "\n";
+
+    sort(Count.begin(),Count.end());
+
+    for(int i=0 ; i<akdmf ;i++){
+        cout << Count[i] << "\n";
+    }
+
+    return 0;
+}
